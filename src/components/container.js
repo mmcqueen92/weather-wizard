66fc045a03eff24cc65d8d190e46fc83f9bbae3d
Malcolm McQueen <110631738+mmcqueen92@users.noreply.github.com>
2023-06-29 15:40:43 -0700

    create basic user location function

--- src/components/container.js
+++ src/components/container.js
@@ -1,28 +1,54 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import {
   getUserLocation,
   getCustomLocation,
 } from "../functions/location-functions";
 
 export default function Container(props) {
-  const [active, setActive] = useState(false);
   const [location, setLocation] = useState();
+  const [weatherData, setWeatherData] = useState();
 
+  //   for testing purposes
+  useEffect(() => {
+    console.log(location);
+  }, [location]);
+
+  // set location to user coords
   const useUserLocation = () => {
-    setLocation(getUserLocation());
-    if (location) {
-      setActive(true);
-    }
+    const userLocation = getUserLocation();
+    console.log("plz: ", userLocation);
+    setLocation(userLocation);
   };
 
+  //   set location based on user input
   const useCustomLocation = (location) => {
     setLocation(getCustomLocation(location));
-    if (location) {
-        setActive(true);
-      }
   };
 
-  if (active) {
+  const getWeatherData = (coords) => {
+    // api call to get weatherdata from coords
+  };
+
+  const checkLocation = () => {
+    console.log(location);
+  };
+
+  function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(updateLocation);
+    } else {
+      console.log("Geolocation is not supported by this browser.")
+    }
+  }
+
+  function updateLocation(position) {
+    console.log("Latitude: " + position.coords.latitude);
+    console.log("Longitude: " + position.coords.longitude);
+    const { latitude, longitude } = position.coords;
+    setLocation({ latitude, longitude })
+  }
+
+  if (location) {
     return (
       <div>
         <h4>This is ACTIVE!</h4>
@@ -34,18 +60,24 @@ export default function Container(props) {
         <div>
           <h5>Hours/Days</h5>
         </div>
+        <div>
+          <h5>Coords?</h5>
+          <div>{location.latitude}</div>
+        </div>
+        <button onClick={checkLocation}>Check Location</button>
       </div>
     );
   } else {
     return (
       <div>
         <h4>This is INACTIVE!</h4>
-        <button onClick={useUserLocation}>User location</button>
+        <button onClick={getLocation}>User location</button>
         <div>
-          <label for="enter_city">Enter a location</label>
+          <label htmlFor="enter_city">Enter a location</label>
           <input type="text" id="enter_city" name="enter_city"></input>
           <button onClick={useCustomLocation}>Custom location</button>
         </div>
+        <button onClick={checkLocation}>Check Location</button>
       </div>
     );
   }
--- src/functions/location-functions.js
+++ src/functions/location-functions.js
@@ -1,20 +1,33 @@
-const getUserLocation = () => {
+const getUserLocation = async () => {
   // get user coords
-  // get address/city from coords
-  // return city/coords depending on API needs
 
+  if ("geolocation" in navigator) {
+    let returnObj = {};
+    navigator.geolocation.getCurrentPosition(({ coords }) => {
+      console.log("big shpoople: ", coords);
+      const { latitude, longitude } = coords;
+      returnObj = { latitude, longitude };
+
+    });
+    return returnObj;
+  } else {
+    console.log("navigator not available");
+    return;
+  }
   //   temp code below
-  console.log("returned user location");
-  return {lat: 49.246292, long: -123.116226}
-}
+  //   console.log("returned user location");
+  //   return {lat: 49.246292, long: -123.116226}
+};
 
 const getCustomLocation = (location) => {
+  // use user input (string) to get coords
 
-    console.log("returned custom location");
-    return {lat: 49.246292, long: -123.116226}
-}
+  //temp code below
+  console.log("returned custom location");
+  return { lat: 49.246292, long: -123.116226 };
+};
 
 module.exports = {
   getUserLocation,
-  getCustomLocation
-}
+  getCustomLocation,
+};
