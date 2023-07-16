145a706de78f93d74f5342889d3354b28a1234a4
Malcolm McQueen <110631738+mmcqueen92@users.noreply.github.com>
2023-07-15 21:16:52 -0700

    initialize tailwindcss

--- client/src/components/forecast-list-item.js
+++ client/src/components/forecast-list-item.js
@@ -1,39 +1,36 @@
-export default function ForecastListItem (props) {
-    // parse time
-    const dateAndTime = props.forecast.dt_txt
-    
-    // 2023-07-15
-    const date = dateAndTime.split(" ")[0]
-    const year = Number(date.split("-")[0])
-    const month = Number(date.split("-")[1])
-    const day = Number(date.split("-")[2])
+export default function ForecastListItem(props) {
+  // parse time
+  const dateAndTime = props.forecast.dt_txt;
 
-    //  03:00:00
-    const time = dateAndTime.split(" ")[1]
-    const hour = Number(time.split(":")[0])
-    const minute = Number(time.split(":")[1])
-    const second = Number(time.split(":")[2])
+  // 2023-07-15
+  const date = dateAndTime.split(" ")[0];
+  const year = Number(date.split("-")[0]);
+  const month = Number(date.split("-")[1]);
+  const day = Number(date.split("-")[2]);
 
-    const utcDate = new Date(Date.UTC(year, month, day, hour, minute, second))
-    const localDate = utcDate.toLocaleString("en-US", {
-        dateStyle: "full",
-        timeStyle: "full"
-    })
+  //  03:00:00
+  const time = dateAndTime.split(" ")[1];
+  const hour = Number(time.split(":")[0]);
+  const minute = Number(time.split(":")[1]);
+  const second = Number(time.split(":")[2]);
 
+  const utcDate = new Date(Date.UTC(year, month, day, hour, minute, second));
+  const localDate = utcDate.toLocaleString("en-US", {
+    dateStyle: "full",
+    timeStyle: "full",
+  });
 
-    return (
-        <div>
-            <h5>Forecast list item:</h5>
-            <div>
-                {props.forecast.dt_txt}<br/>
-                {props.timezone}<br/><br/>
-                <h5>LOCAL DATE/TIME: {localDate}</h5>
-                {props.forecast.main.temp}<br/>
-                {props.forecast.weather[0].description}<br/>
-                
-            </div>
-        </div>
-    )
+  return (
+    <div className="border-2 border-blue-800 m-1 p-1 rounded-md bg-green-200">
+      <br />
+      <h4>Forecast list item:</h4>
+      <div>
+        <h6>LOCAL DATE/TIME: {localDate}</h6>
+        <h6>TEMP: {props.forecast.main.temp}</h6>
+        <h6>DESC: {props.forecast.weather[0].description}</h6>
+      </div>
+    </div>
+  );
 }
 
 // original time
