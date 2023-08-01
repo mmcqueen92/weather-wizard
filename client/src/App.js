9d352837b900e003eb222417c4021532d7e78deb
Malcolm McQueen <110631738+mmcqueen92@users.noreply.github.com>
2023-08-01 14:53:19 -0700

    randomize website background

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
 import "./App.css";
 import Container from './components/container';
+import classNames from "classnames";
 
 
 function App() {
+
+  const backgrounds = ['bg-bingus', 'bg-bongus', 'bg-dingus', 'bg-dongus', 'bg-plingus', 'bg-plongus', 'bg-tringus', 'bg-trongus']
+
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+  const randBg = backgrounds[getRandomInt(8)]
+
+  const appClassNames = classNames('App', randBg, 'bg-cover', 'h-screen', 'p-10')
+
   return (
-    <div className="App bg-lake-island bg-cover h-screen p-10">
+    <div className={appClassNames}>
       <Container/>
     </div>
   );
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,9 +4,17 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'lake-island': "url('/public/img/backgrounds/bg1.jpg')"
+        'bingus': "url('/public/img/backgrounds/bg1.jpg')",
+        'bongus': "url('/public/img/backgrounds/bg2.jpg')",
+        'dingus': "url('/public/img/backgrounds/bg3.jpg')",
+        'dongus': "url('/public/img/backgrounds/bg4.jpg')",
+        'plingus': "url('/public/img/backgrounds/bg5.jpg')",
+        'plongus': "url('/public/img/backgrounds/bg6.jpg')",
+        'tringus': "url('/public/img/backgrounds/bg7.jpg')",
+        'trongus': "url('/public/img/backgrounds/bg8.jpg')",
       }
     },
   },
   plugins: [],
 }
+
