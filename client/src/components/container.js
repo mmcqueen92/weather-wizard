a13014069be72f99e65e2938441dd1649f6055eb
Malcolm McQueen <110631738+mmcqueen92@users.noreply.github.com>
2023-08-08 15:48:08 -0700

    separate state logic and jsx for home component

--- client/src/components/container.js
+++ client/src/components/container.js
@@ -1,15 +1,10 @@
 import { useState, useEffect } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faLocationCrosshairs,
-  faHatWizard,
-} from "@fortawesome/free-solid-svg-icons";
 import getCustomLocation from "../functions/get-custom-location";
 import getCurrentWeather from "../functions/get-current-weather";
 import getForecast from "../functions/get-forecast";
 import getPlaceNameFromCoords from "../functions/get-placename-from-coords";
 import titleCaseString from "../functions/title-case-string";
-
+import Home from "./home";
 import Forecast from "./forecast";
 
 export default function Container(props) {
@@ -69,6 +64,10 @@ export default function Container(props) {
     });
   };
 
+  const handleUserInput = (event) => {
+    setUserInput(event.target.value);
+  };
+
   const back = () => {
     setWeatherData();
   };
@@ -86,48 +85,12 @@ export default function Container(props) {
     );
   } else {
     return (
-      <div className="border-4 border-blue-800 rounded-md max-w-md mx-auto bg-blue-300 p-2">
-        <div className="relative">
-          <h3 className="text-blue-800 font-bold">Welcome to Weather-Wizard</h3>
-          <FontAwesomeIcon
-            icon={faHatWizard}
-            // size="3xl"
-            className="text-blue-800 absolute top-5 right-5 fa-3x"
-          ></FontAwesomeIcon>
-        </div>
-
-        <br />
-        <label htmlFor="user_loc" className="text-blue-800 font-medium">
-          Use current location:{" "}
-        </label>
-        <button
-          name="user_loc"
-          onClick={getLocation}
-          className="border-2 border-blue-800 m-1 p-1 rounded-md h-9 w-9 bg-blue-200 hover:bg-blue-800 hover:text-blue-200 text-blue-800"
-        >
-          <FontAwesomeIcon icon={faLocationCrosshairs}></FontAwesomeIcon>
-        </button>
-        <form onSubmit={useCustomLocation}>
-          <input
-            type="text"
-            id="enter_city"
-            name="enter_city"
-            placeholder="Enter a Location"
-            value={userInput}
-            onChange={(event) => {
-              setUserInput(event.target.value);
-            }}
-            className="border-2 border-blue-800 m-1 p-1 rounded-md"
-            required
-          ></input>
-          <button
-            type="submit"
-            className="border-2 border-blue-800 m-1 p-1 rounded-md bg-blue-200 hover:bg-blue-800 hover:text-blue-200 text-blue-800 font-medium"
-          >
-            Search
-          </button>
-        </form>
-      </div>
+      <Home
+        getLocation={getLocation}
+        useCustomLocation={useCustomLocation}
+        userInput={userInput}
+        handleUserInput={handleUserInput}
+      />
     );
   }
 }
---
+++ client/src/components/home.js
@@ -0,0 +1,50 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faLocationCrosshairs,
+    faHatWizard,
+  } from "@fortawesome/free-solid-svg-icons";
+
+export default function Home(props) {
+const {getLocation, useCustomLocation, userInput, handleUserInput } = props;
+return (
+    <div className="border-4 border-blue-800 rounded-md max-w-md mx-auto bg-blue-300 p-2">
+        <div className="relative">
+          <h3 className="text-blue-800 font-bold">Welcome to Weather-Wizard</h3>
+          <FontAwesomeIcon
+            icon={faHatWizard}
+            className="text-blue-800 absolute top-5 right-5 fa-3x"
+          ></FontAwesomeIcon>
+        </div>
+
+        <br />
+        <label htmlFor="user_loc" className="text-blue-800 font-medium">
+          Use current location:{" "}
+        </label>
+        <button
+          name="user_loc"
+          onClick={getLocation}
+          className="border-2 border-blue-800 m-1 p-1 rounded-md h-9 w-9 bg-blue-200 hover:bg-blue-800 hover:text-blue-200 text-blue-800"
+        >
+          <FontAwesomeIcon icon={faLocationCrosshairs}></FontAwesomeIcon>
+        </button>
+        <form onSubmit={useCustomLocation}>
+          <input
+            type="text"
+            id="enter_city"
+            name="enter_city"
+            placeholder="Enter a Location"
+            value={userInput}
+            onChange={handleUserInput}
+            className="border-2 border-blue-800 m-1 p-1 rounded-md"
+            required
+          ></input>
+          <button
+            type="submit"
+            className="border-2 border-blue-800 m-1 p-1 rounded-md bg-blue-200 hover:bg-blue-800 hover:text-blue-200 text-blue-800 font-medium"
+          >
+            Search
+          </button>
+        </form>
+      </div>
+)
+}
